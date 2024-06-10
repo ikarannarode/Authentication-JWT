564be55c5567435ba8a28588446c0cb539da5be1
Karan <karannarode999@gmail.com>
2024-06-10 21:47:10 +0530

    Some extra functionality added

--- index.js
+++ index.js
@@ -2,98 +2,161 @@ import express from "express"
 import { config } from "dotenv"
 import cookieParser from "cookie-parser"
 import path from "path"
-import mongoose from "mongoose"
+import mongoose, { mongo } from "mongoose"
 import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
-
-// database connection
-
-mongoose.connect("mongodb://localhost:27017", {
-    dbname: "Authentication"
-}).then(() => {
-    console.log("Database connection successful")
-}).catch((err) => {
-    console.log(err)
-})
-//Schema
-const user = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    }
-})
-//Model
-const User = mongoose.model("User", user)
-
-// jwt token
-
-
-//custom middleware
-const isAuthenticated = (req, res, next) => {
-    const { token } = req.cookies;
-    if (token) {
-
-        jwt.verify(token, process.env.SECRET_KEY)
-        next();
-    }
-    else {
-        res.render("login")
-    }
-}
 
 
 
 config({ path: "./config/.env" })
 const app = express()
 
+
+// Database Connection
+
+mongoose.connect("mongodb://localhost:27017", {
+    dbName: "Authentication"
+}).then(() => {
+    console.log("Database Connected Succesfull")
+}).catch((err) => {
+    console.log(err)
+})
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+const User = mongoose.model("User", userSchema)
+
+
+
+
+
+
+// setting up view engine
+app.set("view engine", "ejs")
+
+//Middlewares
+
 app.use(cookieParser())
 app.use(express.static(path.join(path.resolve(), "public")))
 app.use(express.urlencoded({ extended: true }))
 
-app.set("view engine", "ejs")
+//Custom middlewares
+const isAuthenticated = async (req, res, next) => {
+    const { token } = req.cookies
+    if (token) {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY)
+        // console.log(decoded)
+        req.user = await User.findById(decoded);
+
+        next()
+    }
+    else {
+        res.redirect("/login")
+    }
+}
 
 
+
+
+// Routs
 
 
 
 app.post("/login", async (req, res) => {
-    const { name, email } = req.body
-    const users = await User.create({
-        name: name,
-        email: email
-    })
+    const { email, password } = req.body
+    let user = await User.findOne({ email })
+    if (!user) {
+        return res.redirect("/register")
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        return res.render("login", { email, message: "Incorrect Password" })
+    }
 
 
-    const token = jwt.sign({ _id: users._id }, process.env.SECRET_KEY)
 
 
-
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY)
 
     res.cookie("token", token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + 60 * 1000)
+        expires: new Date(Date.now() + 60 * 1000),
+        httpOnly: true
+    })
+    res.redirect("/")
+
+
+})
+
+
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body
+    let user = await User.findOne({ email })
+    if (user) {
+        return res.redirect("/login")
+    }
+    const hashedpassword = await bcrypt.hash(password, 10)
+    user = await User.create({
+        name: name,
+        email: email,
+        password: hashedpassword
+    })
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY)
+
+    res.cookie("token", token, {
+        expires: new Date(Date.now() + 60 * 1000),
+        httpOnly: true
     })
     res.redirect("/")
 })
-app.get("/", isAuthenticated, (req, res) => {
-    res.render('logout')
+
+
+
+
+
+app.get("/login", async (req, res) => {
+    res.render("login")
 })
+
+
+
+
+
+app.get("/", isAuthenticated, (req, res) => {
+
+    res.render("logout", { name: req.user.name })
+});
+
+
+
+
 app.get("/logout", (req, res) => {
     res.cookie("token", null, {
+        httpOnly: true,
         expires: new Date(Date.now())
     })
     res.redirect("/")
 })
 
 
+app.get("/register", (req, res) => {
+    res.render("register")
+})
 
 
 
+
+
+
+
+
+// Server Port
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`)
 })
